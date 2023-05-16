cff2bbc23846dea6321bc21bcf3b83a9dffca6d2
RedietZeraiHadera <redietzeraihadderra@gmail.com>
2023-05-16 23:06:36 +0300

    commit

--- cart.py
+++ cart.py
@@ -1,20 +1,19 @@
 class ShoppingCart:
-    def __init__(self,quantity,price):
+    def __init__(self,quantity,address, payment_method,price,product,category):
         self.products = []
         self.quantity = quantity
-        self.price = price
+        self.adress =address
+        self.payment_method = payment_method
+        self.price=price
+        self.product=product
+        self.category = category
     def add_item(self, product):
         # method to add a product to the shopping cart
         self.products.append(product)
     def remove_item(self, product):
         # method to remove a product from the shopping cart
         self.products.remove(product)
-    def checkout(self, address, payment_method,price, quantity,product,category):
-        self.adress =address
-        self.payment_method = payment_method
-        self.price=price
-        self.quantity=quantity
-        self.product=product
-        self.category = category
+    def checkout(self, quantity,category):
+
         total = category.product.price * quantity
         return total
